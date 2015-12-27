8a1413815757e4eefedae9d098f4ac21f89171d6
abchinguk <allenetie590@gmail.com>
2015-12-27 17:16:43 +0300

    post class + ancestors

---
+++ post.rb
@@ -0,0 +1,34 @@
+class Post
+
+  def initiallize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+     # todo
+  end
+
+  def to_strings
+    # todo
+  end
+
+
+  def save
+    file = File.new(file_path, "w:UTF8")
+
+    for item in to_strings do
+      file.puts(item)
+    end
+
+    file.close
+  end
+
+  def file_path # путь к файлу, куда записывать содержимое объекта
+    current_path = File.dirname(__FILE__)
+
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+
+    return current_path + "/" + file_name
+  end
+end
